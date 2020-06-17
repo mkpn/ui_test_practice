fcaf03c9cae9c65903c7a41946eac05a6df086f1
neesan <exp.ym.lvl@gmail.com>
2020-06-18 03:22:43 +0900

    修正

--- app/src/main/java/makoto/yoshida/uitesttutorial/TestDialogFragment.kt
+++ app/src/main/java/makoto/yoshida/uitesttutorial/TestDialogFragment.kt
@@ -1,7 +1,6 @@
 package makoto.yoshida.uitesttutorial
 
 import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
--- app/src/main/java/makoto/yoshida/uitesttutorial/viewmodel/TestDialogFragmentViewModel.kt
+++ app/src/main/java/makoto/yoshida/uitesttutorial/viewmodel/TestDialogFragmentViewModel.kt
@@ -9,18 +9,17 @@ class TestDialogFragmentViewModel @ViewModelInject constructor (
                                   private val repository: TestRepository) : ViewModel() {
 
     val name = MutableLiveData("変わるかな？？")
-    private val observer = Observer<TestEntity> {
-        name.postValue(it.name)
-    }
 
     fun fetchData() {
         // DialogFragmentのViewModelでLiveDataをobserveできない問題があまりにも深刻でこの方法しか思いつかなかった
         // 素直にTransformations.mapとかしても動かない
         repository.get(1).also { liveData ->
-            liveData.observeForever {
-                observer.onChanged(it)
-                liveData.removeObserver(observer)
-            }
+            liveData.observeForever(object: Observer<TestEntity> {
+                override fun onChanged(t: TestEntity) {
+                    name.postValue(t.name)
+                    liveData.removeObserver(this)
+                }
+            })
         }
     }
 }
